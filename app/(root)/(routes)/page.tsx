a5747ed8ebe136b37e66c6d10f922ccd4887746f
jeong99 <jbh990621@gmail.com>
2024-06-12 17:35:03 +0900

    companion list

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,14 +1,43 @@
 import Categories from "@/components/categories";
+import Companions from "@/components/companions";
 import SearchInput from "@/components/search-input";
 import prismadb from "@/lib/prismadb";
 
-const RootPage = async () => {
+interface RootPageProps {
+  searchParams: {
+    categoryId: string;
+    name: string;
+  };
+}
+
+const RootPage: React.FC<RootPageProps> = async ({ searchParams }) => {
+  const { categoryId, name } = searchParams;
+  const data = await prismadb.companion.findMany({
+    where: {
+      categoryId,
+      name: {
+        search: name,
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true,
+        },
+      },
+    },
+  });
+
   const categories = await prismadb.category.findMany();
 
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
       <Categories data={categories} />
+      <Companions data={data} />
     </div>
   );
 };
